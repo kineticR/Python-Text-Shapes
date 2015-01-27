138e23e116343196b8026989507c91f98760d908
Rhys Dunn <rhysdarambo@gmail.com>
2015-01-27 20:05:14 +0700

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -1,1 +1,25 @@
-pass
+
+Given an integer N draw a square of N x N asterisks, like the ones below
+
+
+def square(n):
+    i = 0
+    while i < n:
+        print(" * " * n)
+        i += 1
+
+
+ *  *  * 
+ *  *  * 
+ *  *  * 
+ 
+ 
+ *  *  *  *  *  *  * 
+ *  *  *  *  *  *  * 
+ *  *  *  *  *  *  * 
+ *  *  *  *  *  *  * 
+ *  *  *  *  *  *  * 
+ *  *  *  *  *  *  * 
+ *  *  *  *  *  *  *
+ 
+ -------------------------------------------
